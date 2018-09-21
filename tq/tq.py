251cd05a743c25f587edfe7bce8ebac7a5753426
Andrew Henry <andrewmichaelhenry@gmail.com>
2018-09-20 22:50:58 -0400

    Add dumb twenty questions functionality to twenty script.

--- tq/tq.py
+++ tq/tq.py
@@ -1,2 +1,18 @@
+NUM_QUESTIONS = 20
+
 def main():
-    pass
+    win = False
+    for i in range(NUM_QUESTIONS):
+        try:
+            response = input('Is it a thing? ')
+        except EOFError:
+            print('\n\nBye!')
+            return
+
+        if response == 'yes':
+            win = True
+            break
+    if win:
+        print('I win!')
+    else:
+        print("You win! But I'm not happy about it...")
