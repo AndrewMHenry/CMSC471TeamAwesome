ef60e2aca050de113d6a841baf5214757340e819
Andrew Henry <andrewmichaelhenry@gmail.com>
2018-09-20 21:39:17 -0400

    Set up minimal Python package with twenty script.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+        name='tq',
+        version='0.0.0',
+        author='Team Awesome',
+        author_email='andrewmichaelhenry@gmail.com',
+        packages=['tq'],
+        include_package_data=True,
+        entry_points={
+            'console_scripts': [
+                'twenty=tq.tq:main',
+                ]
+            }
+        )
+
